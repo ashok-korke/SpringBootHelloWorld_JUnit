92ec52f866383eb93b2361afccff676497b295a4
ashok-korke <ashok.korke14@gmail.com>
2023-08-18 17:53:52 +0530

    updated tests file...

--- src/test/java/com/javainuse/test/WebAppTest.java
+++ src/test/java/com/javainuse/test/WebAppTest.java
@@ -6,8 +6,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
