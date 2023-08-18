b0e92682d2aa412fe1bdd6790e173fd4a15b4748
ashok-korke <ashok.korke14@gmail.com>
2023-08-18 18:25:43 +0530

    updated tests file...

--- src/test/java/com/javainuse/test/WebAppTest.java
+++ src/test/java/com/javainuse/test/WebAppTest.java
@@ -8,15 +8,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+@AutoConfigureMockMvc
+@SpringBootTest
 public class WebAppTest extends SpringBootHelloWorldTest {
 
 	@Autowired
 	private WebApplicationContext webApplicationContext;
-
+	@Autowired
 	private MockMvc mockMvc;
 
 	@Before
