9db87d1fb6793c2648331456af377986ffabbfad
ashok-korke <ashok.korke14@gmail.com>
2023-08-18 17:41:20 +0530

    updated tests file...

--- src/test/java/com/javainuse/test/SpringBootHelloWorldTest.java
+++ src/test/java/com/javainuse/test/SpringBootHelloWorldTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootHelloWorldTests {
+public class SpringBootHelloWorldTest {
 
 	@Test
 	public void contextLoads() {
--- src/test/java/com/javainuse/test/WebAppTest.java
+++ src/test/java/com/javainuse/test/WebAppTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-public class TestWebApp extends SpringBootHelloWorldTests {
+public class WebAppTest extends SpringBootHelloWorldTest {
 
 	@Autowired
 	private WebApplicationContext webApplicationContext;
